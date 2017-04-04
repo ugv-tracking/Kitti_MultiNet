a4f259d108a1ddd4181082a0af44b05144b45c27
maxtom <hitmaxtom@gmail.com>
2017-04-04 14:13:18 +0800

    ok

--- scripts/kitti_nets.py
+++ scripts/kitti_nets.py
@@ -1,26 +1,35 @@
 #! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-
 Copyright (c) 2015 PAL Robotics SL.
 Released under the BSD License.
-
 Created on 7/14/15
-
 @author: Sammy Pfeiffer
-
 test_video_resource.py contains
 a testing code to see if opencv can open a video stream
 useful to debug if video_stream does not work
 """
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+'''
 
+import json
+import logging
+import os
 import cv2
 import sys
+
+sys.path.insert(1, os.path.realpath('../incl/MultiNet/incl'))
+sys.path.insert(1, os.path.realpath('../incl/MultiNet'))
+
+
 
 if __name__ == '__main__':
 
     #resource = sys.argv[1]
-    resource = '/home/xulingyun/codebase/ROS/catkin_ws/src/kitti_player/tusimple.mp4'
+    resource = '/home/xulingyun/tusimple.mp4'
     cap = cv2.VideoCapture(resource)
     if not cap.isOpened():
         print "Error opening resource: " + str(resource)
@@ -37,5 +46,5 @@ if __name__ == '__main__':
         # exit on ESC, you may want to uncomment the print to know which key is ESC for you
         if key == 27 or key == 1048603:
             break
-    cv2.destroyWindow("preview")
+            cv2.destroyWindow("preview")
 
